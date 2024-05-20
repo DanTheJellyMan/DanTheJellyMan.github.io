83b78cdfa36bdc401b23b8852219fb7777b3bd7e
DanTheJellyMan <d3362719@gmail.com>
2024-05-20 02:36:59 -0500

    im sed

--- drawing-board/server.js
+++ drawing-board/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const socketIo = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 let drawingData = [];
 
